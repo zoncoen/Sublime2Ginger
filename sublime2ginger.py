42d9b38a2b28dca94723a02cf4266efddeb5a380
Kenta Mori <heiwabanzai@gmail.com>
2013-07-25 13:01:10 +0900

    Refactoring

--- sublime2ginger.py
+++ sublime2ginger.py
@@ -20,25 +20,37 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+# Sublime Ginger thread
+sublime_ginger_thread = None
+
 
 class GingerGrammarCheker:
-    thread = None
+    def __init__(self, command):
+        """Called when the instance is created.
+        @param command sublime_plugin.TextCommand
+        """
+        # Set original text
+        for region in command.view.sel():
+            region_of_line = command.view.line(region)
+        self.original_text = command.view.substr(region_of_line).lstrip().encode('utf-8')
+        self.sublime_ginger_settings = sublime.load_settings("Sublime2Ginger.sublime-settings")
 
-    def __call__(self, command, edit, original_text):
+    def __call__(self, command, edit):
+        """Called when the instance is called as a function.
+        @param commnd sublime_plugin.TextCommand
+        @param edit sublime.Edit
         """
-        """
-        sublime.set_timeout(lambda: sublime.status_message("Ginger Grammar Checker is running..."), 100)
+        sublime.set_timeout(lambda: sublime.status_message("Ginger grammar checker is running..."), 100)
         # Grammar check with Ginger
-        result, flag = self.parse_result(original_text)
+        result, flag = self.parse_result()
         if flag:
-            output = "Original: " + original_text + "\n" + "Fixed    : " + result
+            output = "Original: " + self.original_text + "\n" + "Fixed    : " + result
             sublime.set_timeout(lambda: self.show_result(command.view.window(), output), 100)
         else:
             sublime.set_timeout(lambda: self.show_result(command.view.window(), result), 100)
 
-    def get_url(self, text):
+    def get_url(self):
         """Get a Ginger URL for checking grammar.
-        @param text English text
         @return URL
         """
         API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
@@ -51,17 +63,16 @@ class GingerGrammarCheker:
             ("lang", "US"),
             ("clientVersion", "2.0"),
             ("apiKey", API_KEY),
-            ("text", text)])
+            ("text", self.original_text)])
         fragment = ""
 
         return(urlparse.urlunparse((scheme, netloc, path, params, query, fragment)))
 
-    def get_result(self, text):
+    def get_result(self):
         """Get a result of checking grammar.
-        @param text English text
         @return result of grammar check by Ginger as JSON
         """
-        url = self.get_url(text)
+        url = self.get_url()
 
         try:
             response = urllib.urlopen(url)
@@ -79,20 +90,21 @@ class GingerGrammarCheker:
 
         return(result)
 
-    def parse_result(self, original_text):
+    def parse_result(self):
         """Grammar check by Ginger.
-        @param text Original English text
         @return result of grammar check by Ginger
         @return status 1(Success)/0(Failure)
         """
-        if len(original_text) > 600:
+        if len(self.original_text) == 0:
+            return("No text :(", 0)
+        elif len(self.original_text) > 600:
             sublime.status_message("You can't check more than 600 characters at a time.")
             return("", 0)
-        fixed_text = original_text
+        fixed_text = self.original_text
 
         # Get JSON as result of grammar check
         try:
-            results = self.get_result(original_text)
+            results = self.get_result()
         except HTTPError as e:
             return("HTTP Error: " + e.code, 0)
         except URLError as e:
@@ -114,7 +126,6 @@ class GingerGrammarCheker:
                 to_index = result["To"] + 1
                 suggest = result["Suggestions"][0]["Text"].encode('utf-8')
 
-                original_text = original_text[:from_index] + original_text[from_index:to_index] + original_text[to_index:]
                 fixed_text = fixed_text[:from_index - fixed_gap] + suggest + fixed_text[to_index - fixed_gap:]
 
                 fixed_gap += to_index - from_index - len(suggest)
@@ -122,6 +133,10 @@ class GingerGrammarCheker:
         return(fixed_text, 1)
 
     def show_result(self, window, output):
+        """Output result into output panel.
+        @param window sublime_plugin.WindowCommand
+        @param output Strings to output.
+        """
         output_view = window.get_output_panel("textarea")
         window.run_command("show_panel", {"panel": "output.textarea"})
 
@@ -131,25 +146,17 @@ class GingerGrammarCheker:
         output_view.end_edit(edit)
         output_view.set_read_only(True)
 
-    def grammer_check(self, command, edit):
-        sublime.status_message("start translate...")
-        # Set region
-        for region in command.view.sel():
-            region_of_line = command.view.line(region)
-        # Grammar check with Ginger
-        original_text = command.view.substr(region_of_line).lstrip().encode('utf-8')
-        if len(original_text) == 0:
-            sublime.status_message("No text.")
-            return
-        if self.thread is not None and self.thread.isAlive() is True:
-            sublime.status_message("Already run grammar check now. Please wait.")
-            return
-        thread = threading.Thread(target=self, args=(command, edit, original_text))
-        thread.setDaemon(True)
-        thread.start()
-
 
 class Sublime2GingerCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        grammer_checker = GingerGrammarCheker()
-        grammer_checker.grammer_check(self, edit)
+        """Function to run Sublime2Ginger plugin.
+        @param edit sublime.Edit
+        """
+        global sublime_ginger_thread
+        grammer_checker = GingerGrammarCheker(self)
+        if sublime_ginger_thread is not None and sublime_ginger_thread.isAlive() is True:
+            sublime.status_message("Already grammar checker is running. Please wait.")
+            return
+        sublime_ginger_thread = threading.Thread(target=grammer_checker, args=(self, edit))
+        sublime_ginger_thread.setDaemon(True)
+        sublime_ginger_thread.start()
